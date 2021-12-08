90b634dc1564fadba95a89d715761b9552d0ad60
Nitin <nsinghboss02@gmail.com>
2021-12-08 19:25:29 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,24 @@
+package com.codechef;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	achieving multithreading with the help of thread class:
+	*/
+    multi1 thread1 = new multi1();
+    multi2 thread2 = new multi2();
+    thread1.start();
+    thread2.start();
+    }
+}
+class multi1 extends Thread{
+    public void run(){
+        System.out.println("thread1 is running");
+    }
+}
+class multi2 extends Thread{
+    public void run(){
+        System.out.println("thread2 is running");
+    }
+}
